23e8f632f6ea1c1286330a585fd3ed09b4c9a21d
tyggerjai <tyggerjai@gmail.com>
2017-01-17 23:42:25 +1100

    Add bash script to checkout and build appropriate esp-idf version

---
+++ esp32/build_upython_esp32.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+# We need ESPIDF for make to run at all
+export ESPIDF="./"
+
+VER=`make idf-version` 
+echo $VER
+IDF_VER=`sed s/.*hash://g <<< $VER` 
+IDF_VER=`sed s/\s//g <<< $IDF_VER` 
+echo "VERSION REQUIRED: -$IDF_VER-"  
+if [ ! -d esp-idf ]
+then
+	git clone --recursive https://github.com/espressif/esp-idf 
+fi 
+cd esp-idf  &&
+GIT_VER=`git -C ./ describe `
+echo $GIT_VER && 
+if [ ! $GIT_VER == $IDF_VER ]
+then
+	git checkout  $IDF_VER 
+fi
+
+GIT_VER=`git -C ./ describe `
+echo $GIT_VER  
+cd ../ 
+IDF_PATH=./esp-idf 
+ESPIDF=./esp-idf 
+make
+
