9ae1e773899bfb76ba5ec55a6b90b3102cee877e
Eric Poulsen <eric@zyxod.com>
2017-03-28 14:32:56 -0700

    esp32/machine_rtc.h: exposes RTC config values to modmachine.c

---
+++ esp32/machine_rtc.h
@@ -0,0 +1,15 @@
+
+#ifndef MICROPY_INCLUDED_ESP32_MACINE_RTC_H
+#define MICROPY_INCLUDED_ESP32_MACINE_RTC_H
+
+typedef struct {
+    uint64_t expiry;    // in microseconds
+    uint64_t ext1_pins; // set bit == pin#
+    int8_t ext0_pin;   // just the pin#, -1 == None
+    bool wake_on_touch : 1;
+    bool ext0_level : 1;
+    bool ext1_level : 1;
+} machine_rtc_config_t;
+
+
+#endif
