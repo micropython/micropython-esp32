d38367add9020363392be12ea730fcf3ec8ef5a4
Anne Jan Brouwer <brouwer@annejan.com>
2017-06-01 21:37:53 +0200

    Lots of primitives

--- esp32/modbadge.c
+++ esp32/modbadge.c
@@ -8,11 +8,14 @@
 #include "badge_pins.h"
 #include "badge_portexp.h"
 #include "badge_touch.h"
+
 #include "font.h"
 #include "font_16px.h"
 #include "font_8px.h"
+
 #include "gde-driver.h"
 #include "gde.h"
+
 #include "imgv2_menu.h"
 #include "imgv2_nick.h"
 #include "imgv2_sha.h"
@@ -79,6 +82,325 @@ STATIC mp_obj_t ugfx_deinit(void) {
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_0(ugfx_deinit_obj, ugfx_deinit);
 
+// PRIMITIVES
+
+/// \method text(x, y, str, colour)
+///
+/// Draw the given text to the position `(x, y)` using the given colour.
+///
+STATIC mp_obj_t ugfx_text(mp_uint_t n_args, const mp_obj_t *args) {
+    // extract arguments
+    //ugfx_obj_t *self = args[0];
+    mp_uint_t len;
+    const char *data = mp_obj_str_get_data(args[2], &len);
+    int x0 = mp_obj_get_int(args[0]);
+    int y0 = mp_obj_get_int(args[1]);
+    int col = mp_obj_get_int(args[3]);
+
+    gdispDrawString(x0, y0, data, gdispOpenFont("Roboto-Regular12"), col);
+
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(ugfx_text_obj, 4, 4, ugfx_text);
+
+
+/// \method line(x1, y1, x2, y2, colour)
+///
+/// Draw a line from (x1,y1) to (x2,y2) using the given colour.
+///
+STATIC mp_obj_t ugfx_line(mp_uint_t n_args, const mp_obj_t *args) {
+    // extract arguments
+    //ugfx_obj_t *self = args[0];
+    int x0 = mp_obj_get_int(args[0]);
+    int y0 = mp_obj_get_int(args[1]);
+	  int x1 = mp_obj_get_int(args[2]);
+    int y1 = mp_obj_get_int(args[3]);
+    int col = mp_obj_get_int(args[4]);
+
+	gdispDrawLine(x0, y0, x1, y1, col);
+
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(ugfx_line_obj, 5, 5, ugfx_line);
+
+
+/// \method thickline(x1, y1, x2, y2, colour, width, round)
+///
+/// Draw a line with a given thickness from (x1,y1) to (x2,y2) using the given colour.
+/// Option to round the ends
+///
+STATIC mp_obj_t ugfx_thickline(mp_uint_t n_args, const mp_obj_t *args) {
+    // extract arguments
+    //ugfx_obj_t *self = args[0];
+    int x0 = mp_obj_get_int(args[0]);
+    int y0 = mp_obj_get_int(args[1]);
+	int x1 = mp_obj_get_int(args[2]);
+    int y1 = mp_obj_get_int(args[3]);
+    int col = mp_obj_get_int(args[4]);
+    int width = mp_obj_get_int(args[5]);
+    bool rnd = (mp_obj_get_int(args[6]) != 0);
+
+	gdispDrawThickLine(x0, y0, x1, y1, col, width, rnd);
+
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(ugfx_thickline_obj, 7, 7, ugfx_thickline);
+
+
+/// \method arc(x1, y1, r, angle1, angle2, colour)
+///
+/// Draw an arc having a centre point at (x1,y1), radius r, using the given colour.
+///
+STATIC mp_obj_t ugfx_arc(mp_uint_t n_args, const mp_obj_t *args) {
+    // extract arguments
+    int x0 = mp_obj_get_int(args[0]);
+    int y0 = mp_obj_get_int(args[1]);
+	int r = mp_obj_get_int(args[2]);
+    int col = mp_obj_get_int(args[5]);
+    int a1 = mp_obj_get_int(args[3]);
+    int a2 = mp_obj_get_int(args[4]);
+
+	gdispDrawArc(x0, y0, r, a1, a2,col);
+
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(ugfx_arc_obj, 6, 6, ugfx_arc);
+
+/// \method fill_arc(x1, y1, r, angle1, angle2, colour)
+///
+/// Fill an arc having a centre point at (x1,y1), radius r, using the given colour.
+///
+STATIC mp_obj_t ugfx_fill_arc(mp_uint_t n_args, const mp_obj_t *args) {
+    // extract arguments
+    int x0 = mp_obj_get_int(args[0]);
+    int y0 = mp_obj_get_int(args[1]);
+	int r = mp_obj_get_int(args[2]);
+	int col = mp_obj_get_int(args[5]);
+    int a1 = mp_obj_get_int(args[3]);
+    int a2 = mp_obj_get_int(args[4]);
+
+	gdispFillArc(x0, y0, r, a1, a2,col);
+
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(ugfx_fill_arc_obj, 6, 6, ugfx_fill_arc);
+
+
+
+/// \method circle(x1, y1, r, colour)
+///
+/// Draw a circle having a centre point at (x1,y1), radius r, using the given colour.
+/// Option to round the ends
+///
+STATIC mp_obj_t ugfx_circle(mp_uint_t n_args, const mp_obj_t *args) {
+    // extract arguments
+    //ugfx_obj_t *self = args[0];
+    int x0 = mp_obj_get_int(args[0]);
+    int y0 = mp_obj_get_int(args[1]);
+	int r = mp_obj_get_int(args[2]);
+    int col = mp_obj_get_int(args[3]);
+
+	gdispDrawCircle(x0, y0, r, col);
+
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(ugfx_circle_obj, 4, 4, ugfx_circle);
+
+/// \method fill_circle(x1, y1, r, colour)
+///
+/// Fill a circle having a centre point at (x1,y1), radius r, using the given colour.
+/// Option to round the ends
+///
+STATIC mp_obj_t ugfx_fill_circle(mp_uint_t n_args, const mp_obj_t *args) {
+    // extract arguments
+    //ugfx_obj_t *self = args[0];
+    int x0 = mp_obj_get_int(args[0]);
+    int y0 = mp_obj_get_int(args[1]);
+	int r = mp_obj_get_int(args[2]);
+    int col = mp_obj_get_int(args[3]);
+
+	gdispFillCircle(x0, y0, r, col);
+
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(ugfx_fill_circle_obj, 4, 4, ugfx_fill_circle);
+
+
+
+/// \method ellipse(x1, y1, a, b, colour)
+///
+/// Draw a ellipse having a centre point at (x1,y1), lengths a,b, using the given colour.
+/// Option to round the ends
+///
+STATIC mp_obj_t ugfx_ellipse(mp_uint_t n_args, const mp_obj_t *args) {
+    // extract arguments
+    //ugfx_obj_t *self = args[0];
+    int x0 = mp_obj_get_int(args[0]);
+    int y0 = mp_obj_get_int(args[1]);
+	int a = mp_obj_get_int(args[2]);
+	int b = mp_obj_get_int(args[3]);
+    int col = mp_obj_get_int(args[4]);
+
+	gdispDrawEllipse(x0, y0, a, b, col);
+
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(ugfx_ellipse_obj, 5, 5, ugfx_ellipse);
+
+/// \method fill_ellipse(x1, y1, a, b, colour)
+///
+/// Fill a ellipse having a centre point at (x1,y1), lengths a,b, using the given colour.
+/// Option to round the ends
+///
+STATIC mp_obj_t ugfx_fill_ellipse(mp_uint_t n_args, const mp_obj_t *args) {
+    // extract arguments
+    //ugfx_obj_t *self = args[0];
+    int x0 = mp_obj_get_int(args[0]);
+    int y0 = mp_obj_get_int(args[1]);
+	int a = mp_obj_get_int(args[2]);
+	int b = mp_obj_get_int(args[3]);
+    int col = mp_obj_get_int(args[4]);
+
+	gdispFillEllipse(x0, y0, a, b, col);
+
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(ugfx_fill_ellipse_obj, 5, 5, ugfx_fill_ellipse);
+
+
+
+
+/// \method polygon(x1, y1, array, colour)
+///
+/// Draw a polygon starting at (x1,y1), using the aarray of points, using the given colour.
+/// Option to round the ends
+///
+STATIC mp_obj_t ugfx_polygon(mp_uint_t n_args, const mp_obj_t *args) {
+    // extract arguments
+    //ugfx_obj_t *self = args[0];
+    int x0 = mp_obj_get_int(args[0]);
+    int y0 = mp_obj_get_int(args[1]);
+	int col = mp_obj_get_int(args[3]);
+
+	point ar[20];
+
+	mp_obj_t *mp_arr;
+	mp_obj_t *mp_arr2;
+	uint len;
+	uint len2;
+	mp_obj_get_array(args[2], &len, &mp_arr);
+
+	if (len <= 20){
+		int i,j;
+		j = 0;
+		for (i = 0; i < len; i++){
+			mp_obj_get_array(mp_arr[i], &len2, &mp_arr2);
+			if (len2 == 2){
+				point p = {mp_obj_get_int(mp_arr2[0]), mp_obj_get_int(mp_arr2[1])};
+				ar[j++] = p;
+			}
+		}
+		gdispDrawPoly(x0, y0, ar, j, col);
+	}
+
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(ugfx_polygon_obj, 4, 4, ugfx_polygon);
+
+
+
+
+/// \method fill_polygon(x1, y1, array, colour)
+///
+/// Draw a polygon starting at (x1,y1), using the aarray of points, using the given colour.
+/// Option to round the ends
+///
+STATIC mp_obj_t ugfx_fill_polygon(mp_uint_t n_args, const mp_obj_t *args) {
+    // extract arguments
+    //ugfx_obj_t *self = args[0];
+    int x0 = mp_obj_get_int(args[0]);
+    int y0 = mp_obj_get_int(args[1]);
+	int col = mp_obj_get_int(args[3]);
+
+	point ar[20];
+
+	mp_obj_t *mp_arr;
+	mp_obj_t *mp_arr2;
+	uint len;
+	uint len2;
+	mp_obj_get_array(args[2], &len, &mp_arr);
+
+	if (len <= 20){
+		int i,j;
+		j = 0;
+		for (i = 0; i < len; i++){
+			mp_obj_get_array(mp_arr[i], &len2, &mp_arr2);
+			if (len2 == 2){
+				point p = {mp_obj_get_int(mp_arr2[0]), mp_obj_get_int(mp_arr2[1])};
+				ar[j++] = p;
+			}
+		}
+		gdispFillConvexPoly(x0, y0, ar, i, col);
+	}
+
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(ugfx_fill_polygon_obj, 4, 4, ugfx_fill_polygon);
+
+
+
+
+/// \method area(x1, y1, a, b, colour)
+///
+/// Fill area from (x,y), with lengths x1,y1, using the given colour.
+///
+STATIC mp_obj_t ugfx_area(mp_uint_t n_args, const mp_obj_t *args) {
+    // extract arguments
+    //ugfx_obj_t *self = args[0];
+    int x0 = mp_obj_get_int(args[0]);
+    int y0 = mp_obj_get_int(args[1]);
+	int a = mp_obj_get_int(args[2]);
+	int b = mp_obj_get_int(args[3]);
+    int col = mp_obj_get_int(args[4]);
+
+	gdispFillArea(x0, y0, a, b, col);
+
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(ugfx_area_obj, 5, 5, ugfx_area);
+
+/// \method clear(color=ugfx.WHITE)
+///
+/// Clear screen
+///
+STATIC mp_obj_t ugfx_clear(mp_uint_t n_args, const mp_obj_t *args) {
+    int color = n_args == 0 ? White : mp_obj_get_int(args[0]);
+    gdispFillArea(0, 0, gdispGetWidth(), gdispGetHeight(), color);
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(ugfx_clear_obj, 0, 1, ugfx_clear);
+
+/// \method box(x1, y1, a, b, colour)
+///
+/// Draw a box from (x,y), with lengths x1,y1, using the given colour.
+///
+STATIC mp_obj_t ugfx_box(mp_uint_t n_args, const mp_obj_t *args) {
+    // extract arguments
+    //ugfx_obj_t *self = args[0];
+    int x0 = mp_obj_get_int(args[0]);
+    int y0 = mp_obj_get_int(args[1]);
+	int a = mp_obj_get_int(args[2]);
+	int b = mp_obj_get_int(args[3]);
+    int col = mp_obj_get_int(args[4]);
+
+	gdispDrawBox(x0, y0, a, b, col);
+
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(ugfx_box_obj, 5, 5, ugfx_box);
+
+// DEMO
+
 STATIC mp_obj_t ugfx_demo(mp_obj_t hacking) {
   font_t robotoBlackItalic;
   font_t permanentMarker;
@@ -118,6 +440,25 @@ STATIC const mp_rom_map_elem_t badge_module_globals_table[] = {
 
     { MP_OBJ_NEW_QSTR(MP_QSTR_ugfx_init), (mp_obj_t)&ugfx_init_obj },
     { MP_OBJ_NEW_QSTR(MP_QSTR_ugfx_deinit), (mp_obj_t)&ugfx_deinit_obj },
+
+    { MP_OBJ_NEW_QSTR(MP_QSTR_BLACK),      MP_OBJ_NEW_SMALL_INT(Black) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_WHITE),      MP_OBJ_NEW_SMALL_INT(White) },
+
+    { MP_OBJ_NEW_QSTR(MP_QSTR_text), (mp_obj_t)&ugfx_text_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_line), (mp_obj_t)&ugfx_line_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_box), (mp_obj_t)&ugfx_box_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_area), (mp_obj_t)&ugfx_area_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_clear), (mp_obj_t)&ugfx_clear_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_thickline), (mp_obj_t)&ugfx_thickline_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_circle), (mp_obj_t)&ugfx_circle_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_fill_circle), (mp_obj_t)&ugfx_fill_circle_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_ellipse), (mp_obj_t)&ugfx_ellipse_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_fill_ellipse), (mp_obj_t)&ugfx_fill_ellipse_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_arc), (mp_obj_t)&ugfx_arc_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_fill_arc), (mp_obj_t)&ugfx_fill_arc_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_polygon), (mp_obj_t)&ugfx_polygon_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_fill_polygon), (mp_obj_t)&ugfx_fill_polygon_obj },
+
     { MP_OBJ_NEW_QSTR(MP_QSTR_ugfx_demo), (mp_obj_t)&ugfx_demo_obj },
 };
 
