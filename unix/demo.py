7bd3c17c800c279b9745f8238aa8ac803db7ef9b
Anne Jan Brouwer <brouwer@annejan.com>
2017-06-12 13:05:49 +0200

    Fixed demo code

--- unix/demo.py
+++ unix/demo.py
@@ -2,22 +2,22 @@ import ugfx
 
 ugfx.init()
 
-ugfx.clear(badge.BLACK)
+ugfx.clear(ugfx.BLACK)
 
-ugfx.fill_circle(60, 60, 50, badge.WHITE);
-ugfx.fill_circle(60, 60, 40, badge.BLACK);
-ugfx.fill_circle(60, 60, 30, badge.WHITE);
-ugfx.fill_circle(60, 60, 20, badge.BLACK);
-ugfx.fill_circle(60, 60, 10, badge.WHITE);
+ugfx.fill_circle(60, 60, 50, ugfx.WHITE);
+ugfx.fill_circle(60, 60, 40, ugfx.BLACK);
+ugfx.fill_circle(60, 60, 30, ugfx.WHITE);
+ugfx.fill_circle(60, 60, 20, ugfx.BLACK);
+ugfx.fill_circle(60, 60, 10, ugfx.WHITE);
 
-ugfx.thickline(1,1,100,100,badge.WHITE,10,5)
-ugfx.box(30,30,50,50,badge.WHITE)
+ugfx.thickline(1,1,100,100,ugfx.WHITE,10,5)
+ugfx.box(30,30,50,50,ugfx.WHITE)
 
-ugfx.string(150,25,"STILL","Roboto_BlackItalic24",badge.WHITE)
-ugfx.string(130,50,"Hacking","PermanentMarker22",badge.WHITE)
+ugfx.string(150,25,"STILL","Roboto_BlackItalic24",ugfx.WHITE)
+ugfx.string(130,50,"Hacking","PermanentMarker22",ugfx.WHITE)
 len = ugfx.get_string_width("Hacking","PermanentMarker22")
-ugfx.line(130, 72, 144 + len, 72, badge.WHITE)
-ugfx.line(140 + len, 52, 140 + len, 70, badge.WHITE)
-ugfx.string(140,75,"Anyway","Roboto_BlackItalic24",badge.WHITE)
+ugfx.line(130, 72, 144 + len, 72, ugfx.WHITE)
+ugfx.line(140 + len, 52, 140 + len, 70, ugfx.WHITE)
+ugfx.string(140,75,"Anyway","Roboto_BlackItalic24",ugfx.WHITE)
 
 ugfx.flush()
