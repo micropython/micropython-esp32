485007de5180cb7b990b1b229b1194ad2db15ac7
Anne Jan Brouwer <brouwer@annejan.com>
2017-06-15 15:58:55 +0200

    Disabled MICROPY_PY_SYS_STDIO_BUFFER, minor cleanup

--- esp32/mpconfigport.h
+++ esp32/mpconfigport.h
@@ -105,7 +105,7 @@
 #define MICROPY_PY_SYS_MODULES              (1)
 #define MICROPY_PY_SYS_EXIT                 (1)
 #define MICROPY_PY_SYS_STDFILES             (1)
-#define MICROPY_PY_SYS_STDIO_BUFFER         (1)
+#define MICROPY_PY_SYS_STDIO_BUFFER         (0)
 #define MICROPY_PY_UERRNO                   (1)
 #define MICROPY_PY_USELECT                  (1)
 #define MICROPY_PY_UTIME_MP_HAL             (1)
