dec49b3329f09a18d2a057d5e3742ce17ab5584d
Aram <aram@factorit.nl>
2017-06-22 01:00:10 +0200

    Actual command that works uses flags -dcs

--- esp32/romfs_files.h
+++ esp32/romfs_files.h
@@ -2,6 +2,6 @@
  * This file contains the list of files for the ROMFS.
  *
  * The files have been converted using...
- * 		file2c -dbcs infile outfile
+ * 		file2c -dcs infile outfile
  */
 #include "imgv3_hacking.h"
