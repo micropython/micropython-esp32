3dec97ccd2cfbdbb93f2064e5a355a7040d94af7
Anne Jan Brouwer <brouwer@annejan.com>
2017-06-22 10:46:51 +0200

    Yah it's lame

---
+++ esp32/modules/installer.py
@@ -0,0 +1,125 @@
+import ugfx
+import sys
+import gc
+import uos as os
+import uerrno as errno
+import ujson as json
+import ussl
+import usocket
+import time
+import network
+
+sta_if = network.WLAN(network.STA_IF); sta_if.active(True)
+# sta_if.connect('real-internetz', 'Zigg0sucks')
+sta_if.connect('SORRY!', '@mst3rd@m')
+
+ugfx.init()
+
+ugfx.clear(ugfx.BLACK)
+ugfx.string(20,25,"Connecting to:","Roboto_BlackItalic24",ugfx.WHITE)
+ugfx.string(140,75, "WiFi","PermanentMarker22",ugfx.WHITE)
+ugfx.flush()
+
+while not sta_if.isconnected():
+    time.sleep(0.1)
+    pass
+
+ugfx.clear(ugfx.BLACK)
+ugfx.string(180,25,"STILL","Roboto_BlackItalic24",ugfx.WHITE)
+ugfx.string(160,50,"Hacking","PermanentMarker22",ugfx.WHITE)
+len = ugfx.get_string_width("Hacking","PermanentMarker22")
+ugfx.line(160, 72, 144 + len, 72, ugfx.WHITE)
+ugfx.line(170 + len, 52, 140 + len, 70, ugfx.WHITE)
+ugfx.string(170,75,"Anyway","Roboto_BlackItalic24",ugfx.WHITE)
+
+ugfx.input_init()
+
+debug = False
+warn_ussl = True
+
+def url_open(url):
+    global warn_ussl
+
+    if debug:
+        print(url)
+
+    proto, _, host, urlpath = url.split('/', 3)
+    try:
+        ai = usocket.getaddrinfo(host, 443)
+    except OSError as e:
+        fatal("Unable to resolve %s (no Internet?)" % host, e)
+    #print("Address infos:", ai)
+    addr = ai[0][4]
+
+    s = usocket.socket(ai[0][0])
+    try:
+        #print("Connect address:", addr)
+        s.connect(addr)
+
+        if proto == "https:":
+            s = ussl.wrap_socket(s, server_hostname=host)
+            if warn_ussl:
+                print("Warning: %s SSL certificate is not validated" % host)
+                warn_ussl = False
+
+        # MicroPython rawsocket module supports file interface directly
+        s.write("GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n" % (urlpath, host))
+        l = s.readline()
+        protover, status, msg = l.split(None, 2)
+        if status != b"200":
+            if status == b"404" or status == b"301":
+                raise NotFoundError("Package not found")
+            raise ValueError(status)
+        while 1:
+            l = s.readline()
+            if not l:
+                raise ValueError("Unexpected EOF in HTTP headers")
+            if l == b'\r\n':
+                break
+    except Exception as e:
+        s.close()
+        raise e
+
+    return s
+
+options = ugfx.List(0,0,int(ugfx.width()/2),ugfx.height())
+
+f = url_open("https://badge.sha2017.org/eggs/list/json")
+try:
+    packages = json.load(f)
+finally:
+    f.close()
+
+for package in packages:
+    options.add_item("%s rev. %s"% (package["name"], package["revision"]))
+
+ugfx.flush()
+
+def show_description(active):
+    if active:
+         text.text(packages[options.selected_index()]["description"])
+    ugfx.flush()
+
+def woezel_it(active):
+    if active:
+        ugfx.clear(ugfx.BLACK)
+        ugfx.string(40,25,"Installing:","Roboto_BlackItalic24",ugfx.WHITE)
+        ugfx.string(100,75, packages[options.selected_index()]["name"],"PermanentMarker22",ugfx.WHITE)
+        ugfx.flush()
+        import woezel
+        woezel.install(packages[options.selected_index()]["slug"])
+        ugfx.clear(ugfx.BLACK)
+        ugfx.string(40,25,"Running:","Roboto_BlackItalic24",ugfx.WHITE)
+        ugfx.string(100,75, packages[options.selected_index()]["name"],"PermanentMarker22",ugfx.WHITE)
+        ugfx.flush()
+        app = __import__(packages[options.selected_index()]["slug"])
+
+ugfx.input_attach(ugfx.JOY_UP, show_description)
+ugfx.input_attach(ugfx.JOY_DOWN, show_description)
+ugfx.input_attach(ugfx.BTN_A, woezel_it)
+ugfx.input_attach(ugfx.BTN_B, woezel_it)
+
+ugfx.input_attach(ugfx.JOY_LEFT, lambda x: ugfx.flush())
+ugfx.input_attach(ugfx.JOY_RIGHT, lambda x: ugfx.flush())
+
+text = ugfx.Textbox(int(ugfx.width()/2),0, int(ugfx.width()/2), ugfx.height())
