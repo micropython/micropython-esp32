b52052f146382ab23c8d2006530cb30e09f821ef
Anne Jan Brouwer <brouwer@annejan.com>
2017-07-14 12:21:00 +0200

    Slash => Launcher wait for flush

--- esp32/modules/splash.py
+++ esp32/modules/splash.py
@@ -18,25 +18,25 @@ def draw_logo(x,y,h):
     ugfx.line(x, y + 47, x + 14 + len, y + 47, ugfx.BLACK)
     ugfx.line(x + 10 + len, y + 27, x + 10 + len, y + 45, ugfx.BLACK)
     ugfx.string(x + 10, y + 50,"Anyway","Roboto_BlackItalic24",ugfx.BLACK)
-    
+
 def draw_home(percent, cstate, status):
     ugfx.clear(ugfx.WHITE)
     if (cstate):
         ugfx.string(0,  0, str(percent)+"% & Charging... | "+status,"Roboto_Regular12",ugfx.BLACK)
     else:
         ugfx.string(0,  0, str(percent)+"% | "+status,"Roboto_Regular12",ugfx.BLACK)
-    
+
     htext = badge.nvs_get_str("owner", "htext", "")
     if (htext!=""):
       draw_logo(160, 25, htext)
-    
+
     nick = badge.nvs_get_str("owner", "name", "Anonymous")
     ugfx.string(0, 40, nick, "PermanentMarker36", ugfx.BLACK)
-    
+
     ugfx.set_lut(ugfx.LUT_FULL)
     ugfx.flush()
     ugfx.set_lut(ugfx.LUT_FASTER)
-    
+
 def draw_batterylow(percent):
     ugfx.clear(ugfx.WHITE)
     ugfx.string(0,  0, str(percent)+"%  - BATTERY LOW, PLEASE CHARGE!","Roboto_Regular12",ugfx.BLACK)
@@ -45,77 +45,76 @@ def draw_batterylow(percent):
     ugfx.string(0, 40, nick, "PermanentMarker36", ugfx.BLACK)
     ugfx.set_lut(ugfx.LUT_FASTER)
     ugfx.flush()
-    
+
 def start_launcher(pushed):
     global splashTimer
     if(pushed):
         splashTimer.deinit()
-        
+
         #By importing it...
         #import launcher
-        
+
         #Or by sleeping...
         ugfx.clear(ugfx.WHITE)
         ugfx.string(0,  0, "Starting launcher...","Roboto_Regular12",ugfx.BLACK)
         ugfx.flush()
         import time
-        time.sleep(0.5)
+        badge.eink_busy_wait()
         import esp
         esp.rtcmem_write_string("launcher")
         esp.start_sleeping(1)
-        
+
 def gotosleep():
     import time
     time.sleep(1)
     import deepsleep
     deepsleep.start_sleeping(120000)
-        
+
 def splashTimer_callback(tmr):
     # Battery status
     cstate = badge.battery_charge_status()
     vbatt = badge.battery_volt_sense()
     percent = battery_percent(3800, 4300, vbatt)
-    
+
     # Depending on battery status...
     if (cstate) or (percent>95): # Charging or full...
-        draw_home(percent, cstate, "Press START!") 
+        draw_home(percent, cstate, "Press START!")
     else: # If on battery power...
         if (percent<10): # ... and battery is empty
-           draw_batterylow(percent) 
+           draw_batterylow(percent)
            ugfx.flush()
         else: # ... and battery is okay
            draw_home(percent, cstate, "Badge is sleeping...")
-           
+
         gotosleep()
-            
-def splash_main():   
+
+def splash_main():
     # Battery status
     cstate = badge.battery_charge_status()
     vbatt = badge.battery_volt_sense()
     percent = battery_percent(3800, 4300, vbatt)
-        
+
     # Init graphics and show homescreen
     ugfx.init()
-    
+
     # Depending on battery status...
     if (cstate) or (percent>9):
         draw_home(percent, cstate, "Press START!")
-                
+
         # Accept input
         ugfx.input_init()
         ugfx.input_attach(ugfx.BTN_START, start_launcher)
-        
+
         # Start timer
         global splashTimer
         splashTimer.init(period=5000, mode=Timer.PERIODIC, callback=splashTimer_callback)
-        
+
     else:
-        draw_batterylow(percent)       
+        draw_batterylow(percent)
         gotosleep()
-  
+
 # Globals
 splashTimer = Timer(-1)
 
 # Start main application
 splash_main()
- 
