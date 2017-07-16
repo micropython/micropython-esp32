317166bf7d35f617fc49d1043fc2a1308579eeb5
Renze Nicolai <renze@rnplus.nl>
2017-07-16 22:17:58 +0200

    Add timeout for wifi connection

--- esp32/modules/installer.py
+++ esp32/modules/installer.py
@@ -17,6 +17,13 @@ ugfx.flush()
 timeout = 500
 while not wifi.sta_if.isconnected():
     time.sleep(0.1)
+    timeout = timeout - 1
+    if (timeout<1):
+        ugfx.clear(ugfx.BLACK)
+        ugfx.string(5,5,"Failure.","Roboto_BlackItalic24",ugfx.WHITE)
+        ugfx.flush()
+        time.sleep(2)
+        appglue.start_app("")
     pass
 
 ugfx.clear(ugfx.WHITE)
