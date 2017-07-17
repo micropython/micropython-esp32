fed1915e899c437064859509878cef7a0ee8d473
Anne Jan Brouwer <brouwer@annejan.com>
2017-07-17 18:51:27 +0200

    Configurable BPP

--- esp32/main.c
+++ esp32/main.c
@@ -144,23 +144,23 @@ void app_main(void) {
   uint8_t magic = esp_rtcmem_read(0);
   uint8_t inv_magic = esp_rtcmem_read(1);
 
-  if (false) {
-  	//Grab level of int pin of touchpad. If high, this was a
-  	//scheduled wakeup because of a deep sleep timeout. If low,
-  	//the user used the touchpad.
-  	//yes, this is v1 specific. Please add v0.x support yourself.
-  	gpio_config_t io_conf = {
-  		.mode         = GPIO_MODE_INPUT,
-  		.pin_bit_mask = 1LL << PIN_NUM_MPR121_INT,
-  		.pull_down_en = 0,
-  		.pull_up_en   = 1,
-  	};
-  	gpio_config(&io_conf);
-  	if (gpio_get_level(PIN_NUM_MPR121_INT)==1) {
-  		printf("Touch int is high. Starting bpp.\n");
-  		do_bpp_bgnd();
-  	}
-  }
+#ifdef CONFIG_SHA_BPP_ENABLE
+	//Grab level of int pin of touchpad. If high, this was a
+	//scheduled wakeup because of a deep sleep timeout. If low,
+	//the user used the touchpad.
+	//yes, this is v1 specific. Please add v0.x support yourself.
+	gpio_config_t io_conf = {
+		.mode         = GPIO_MODE_INPUT,
+		.pin_bit_mask = 1LL << PIN_NUM_MPR121_INT,
+		.pull_down_en = 0,
+		.pull_up_en   = 1,
+	};
+	gpio_config(&io_conf);
+	if (gpio_get_level(PIN_NUM_MPR121_INT)==1) {
+		printf("Touch int is high. Starting bpp.\n");
+		do_bpp_bgnd();
+	}
+#endif
 
   if (magic == (uint8_t)~inv_magic) {
     printf("Magic checked out!\n");
