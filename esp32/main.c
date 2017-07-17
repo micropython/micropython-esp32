ff390285a0b3d811037fca079ad5115c6a32eea1
Anne Jan Brouwer <brouwer@annejan.com>
2017-07-17 16:19:20 +0200

    if (false) bpp component

--- esp32/main.c
+++ esp32/main.c
@@ -141,36 +141,38 @@ void do_bpp_bgnd() {
 void app_main(void) {
    badge_base_init();
 
-    uint8_t magic = esp_rtcmem_read(0);
-    uint8_t inv_magic = esp_rtcmem_read(1);
+  uint8_t magic = esp_rtcmem_read(0);
+  uint8_t inv_magic = esp_rtcmem_read(1);
 
-	// //Grab level of int pin of touchpad. If high, this was a
-	// //scheduled wakeup because of a deep sleep timeout. If low,
-	// //the user used the touchpad.
-	// //yes, this is v1 specific. Please add v0.x support yourself.
-	// gpio_config_t io_conf = {
-	// 	.mode         = GPIO_MODE_INPUT,
-	// 	.pin_bit_mask = 1LL << PIN_NUM_MPR121_INT,
-	// 	.pull_down_en = 0,
-	// 	.pull_up_en   = 1,
-	// };
-	// gpio_config(&io_conf);
-	// if (gpio_get_level(PIN_NUM_MPR121_INT)==1) {
-	// 	printf("Touch int is high. Starting bpp.\n");
-	// 	do_bpp_bgnd();
-	// }
+  if (false) {
+  	//Grab level of int pin of touchpad. If high, this was a
+  	//scheduled wakeup because of a deep sleep timeout. If low,
+  	//the user used the touchpad.
+  	//yes, this is v1 specific. Please add v0.x support yourself.
+  	gpio_config_t io_conf = {
+  		.mode         = GPIO_MODE_INPUT,
+  		.pin_bit_mask = 1LL << PIN_NUM_MPR121_INT,
+  		.pull_down_en = 0,
+  		.pull_up_en   = 1,
+  	};
+  	gpio_config(&io_conf);
+  	if (gpio_get_level(PIN_NUM_MPR121_INT)==1) {
+  		printf("Touch int is high. Starting bpp.\n");
+  		do_bpp_bgnd();
+  	}
+  }
 
-    if (magic == (uint8_t)~inv_magic) {
-      printf("Magic checked out!\n");
-        switch (magic) {
-          case 1:
-            printf("Starting OTA\n");
-            sha2017_ota_update();
-        }
-    } else {
-      xTaskCreateStaticPinnedToCore(mp_task, "mp_task", MP_TASK_STACK_LEN, NULL, MP_TASK_PRIORITY,
-                                    &mp_task_stack[0], &mp_task_tcb, 0);
-    }
+  if (magic == (uint8_t)~inv_magic) {
+    printf("Magic checked out!\n");
+      switch (magic) {
+        case 1:
+          printf("Starting OTA\n");
+          sha2017_ota_update();
+      }
+  } else {
+    xTaskCreateStaticPinnedToCore(mp_task, "mp_task", MP_TASK_STACK_LEN, NULL, MP_TASK_PRIORITY,
+                                  &mp_task_stack[0], &mp_task_tcb, 0);
+  }
 }
 
 void nlr_jump_fail(void *val) {
