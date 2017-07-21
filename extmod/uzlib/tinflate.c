2b5d93822a68eeb06c107d64a0c4249373402806
Anne Jan Brouwer <brouwer@annejan.com>
2017-07-21 15:46:36 +0200

    Only include and use ESP_LOGE on ESP32 build

--- extmod/uzlib/tinflate.c
+++ extmod/uzlib/tinflate.c
@@ -35,7 +35,9 @@
 #include <assert.h>
 #include "tinf.h"
 
+#ifdef ESP32
 #include <esp_log.h>
+#endif
 
 uint32_t tinf_get_le_uint32(TINF_DATA *d);
 uint32_t tinf_get_be_uint32(TINF_DATA *d);
@@ -364,7 +366,9 @@ static int tinf_inflate_block_data(TINF_DATA *d, TINF_TREE *lt, TINF_TREE *dt)
         offs = tinf_read_bits(d, dist_bits[dist], dist_base[dist]);
         if (d->dict_ring) {
             if (offs > d->dict_size) {
+                #ifdef ESP32
                 ESP_LOGE("extmod/uzlib/tinflate", "dictionary not large enough! offs=%d, dict=%d", offs, d->dict_size);
+                #endif
                 return TINF_DICT_ERROR;
             }
             d->lzOff = d->dict_idx - offs;
