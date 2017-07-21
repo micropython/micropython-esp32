46cfa0ec66a64cabecf8dc3e64f620335c4596cd
Anne Jan Brouwer <brouwer@annejan.com>
2017-07-21 14:42:59 +0200

    badge.eink_png instead of ugfx.display_image,  might need to move taht for consistency

--- esp32/modules/sponsors.py
+++ esp32/modules/sponsors.py
@@ -7,7 +7,7 @@ def show_sponsors():
     for x in range(1, 5):
         ugfx.clear(ugfx.WHITE)
         try:
-            ugfx.display_image(0,0,'/sponsors/sponsor%s.png' % x)
+            badge.eink_png(0,0,'/sponsors/sponsor%s.png' % x)
         except:
             ugfx.string(0, 0, "SPONSOR LOAD ERROR #"+str(x), "Roboto_Regular12", ugfx.BLACK)
         ugfx.flush()
