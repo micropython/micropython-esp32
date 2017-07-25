7259b7e53284accadb74e998f70efd39f023f53d
Roosted7 <thomasroos@live.nl>
2017-07-25 20:48:14 +0200

    Optional rebooting screen appglue

--- esp32/modules/appglue.py
+++ esp32/modules/appglue.py
@@ -1,14 +1,14 @@
 import ugfx, esp, badge, deepsleep
 
-def start_app(app):
-    ugfx.clear(ugfx.WHITE)
-    ugfx.string(0, 0, "Rebooting..", "PermanentMarker22", ugfx.BLACK)
-    if (app==""):
-        ugfx.string(0,  25, "Returning to homescreen...","Roboto_Regular12",ugfx.BLACK)
-    else:
-        ugfx.string(0,  25, "Starting "+app+"...","Roboto_Regular12",ugfx.BLACK)
-    ugfx.set_lut(ugfx.LUT_FASTEST)
-    ugfx.flush()
+def start_app(app, display = True):
+    if display:
+        ugfx.clear(ugfx.WHITE)
+        ugfx.string(0, 0, "Loading...", "PermanentMarker22", ugfx.BLACK)
+        if app:
+            ugfx.string(0,  25, "Starting "+app+"...","Roboto_Regular12",ugfx.BLACK)
+        else:
+            ugfx.string(0,  25, "Returning to homescreen...","Roboto_Regular12",ugfx.BLACK)
+        ugfx.flush(ugfx.LUT_FASTEST)
     esp.rtcmem_write_string(app)
     badge.eink_busy_wait()
     deepsleep.reboot()
